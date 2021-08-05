3f0ffe24dce86aadfb5961bc101269a53a4e52f0
stutisaurya <stuti.saurya@gmail.com>
2021-08-05 17:07:26 +0530

    Added:Single Dit Number

---
+++ CaseProgram/SingleDigit.js
@@ -0,0 +1,38 @@
+let number = Math.floor(Math.random() * 10);
+let day;
+switch (number) {
+    case 0: 
+        day = 'zero';
+        break;
+    case 1:
+        day = 'one';
+        break;
+    case 2:
+        day = 'two';
+        break;
+    case 3:
+        day = 'three';
+        break;
+    case 4:
+        day = 'four';
+        break;
+    case 5:
+        day = 'five';
+        break;
+    case 6:
+        day = 'six';
+        break;
+    case 7:
+        day = 'seven';
+        break;
+    case 7:
+        day = 'eight';
+        break;
+    case 8:
+        day = 'nine';
+        break;
+    default: 
+        break;
+}
+
+console.log('Ouput is : '+day);
